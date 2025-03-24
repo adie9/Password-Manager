5f8c0f9812a4dcf557e21c151251fd08d701c960
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-03-23 21:31:05 -0400

    Testing password encryption and decryption

--- app.py
+++ app.py
@@ -9,21 +9,31 @@ import os, sqlite3
 #def it_inventory():
 #    return "<h1>Password Manager</h1>"
 
-def encrypt_password(password):
+def encrypt_password(password, key):
     password = password.encode()
-    aes_key = get_random_bytes(16)
-    cipher = AES.new(aes_key, AES.MODE_EAX)
+    cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(password)
-    with open("encrypted.bin", wb) as file:
-        file.write(tag)
-        file.write(cipher.nonce)
-        file.write(ciphertext)
-    return cipher
-    
-def decrypt_password(password, key):
-    
+    nonce = cipher.nonce
+    return nonce + tag + ciphertext
+
+def decrypt_password(encrypted_password, key):
+    nonce = encrypted_password[:16]
+    tag = encrypted_password[16:32]
+    ciphertext = encrypted_password[32:]
+
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    decrypted_password = cipher.decrypt_and_verify(ciphertext, tag)
+    return decrypted_password.decode()
+
+def save_password(service, username, password):
     pass
+
 
 # Test Case
 pass_word = "testpass011"
-print(encrypt_password(pass_word))
+aes_key = get_random_bytes(16)
+encrypted_password = encrypt_password(pass_word, aes_key)
+decrypted_password = decrypt_password(encrypted_password, aes_key)
+print(encrypted_password)
+print(decrypted_password)
+
