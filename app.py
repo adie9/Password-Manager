28539a5c25ca19990b5bb8130223d862008691e5
Adrian Guerra <70285960+adie9@users.noreply.github.com>
2025-03-25 15:48:45 -0400

    Added comments and deleted .bin file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
-import os, sqlite3, getpass
+import sqlite3, getpass
 
 # Encrypting password using AES mode GCM
 def encrypt_password(password, key):
@@ -8,26 +8,32 @@ def encrypt_password(password, key):
     password = password.encode()
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
     ciphertext, tag = cipher.encrypt_and_digest(password)
-    return nonce + tag + ciphertext
+    return nonce + tag + ciphertext # Concatenating nonce, tag, and ciphertext for retrieval later
 
+# Decrypting password using AES mode GCM
 def decrypt_password(encrypted_password, key):
-    encrypted_password = encrypted_password[0] # Setting variable to binary inside tuple
+    encrypted_password = encrypted_password[0] # Accessing binary value inside tuple
+
+    # Accessing nonce, tag, and ciphertext by splicing their respective lengths from encrypted_password
     nonce = encrypted_password[:12]
     tag = encrypted_password[12:28]
     ciphertext = encrypted_password[28:]
+
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
     password = cipher.decrypt_and_verify(ciphertext, tag)
     return password.decode()
 
+# Function that saves password based on service, username, and password input
 def save_password(service, username, password):
-    pass_word = encrypt_password(password, aes_key)
+    pass_word = encrypt_password(password, aes_key) # Passing user password to function for encryption
     print("Saving password...")
 
     conn = sqlite3.connect("password_storage.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM passwords WHERE (service, username) = (?, ?)", (service, username))
+    cursor.execute("SELECT * FROM passwords WHERE (service, username) = (?, ?)", (service, username)) 
     results = cursor.fetchall()
 
+    # Checking if the (service, username) pair already exists in the database
     if results:
         update = input("It seems that username is already bound to the specified service. Would you like to update its password? (Enter 'yes' or 'no'): ")
         match update.lower():
@@ -42,13 +48,15 @@ def save_password(service, username, password):
     conn.commit()
     conn.close()
 
-
+# Function that deletes password based on service and username input
 def delete_password(service, username):
     print("Deleting password...")
     conn = sqlite3.connect("password_storage.db")
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM passwords WHERE (service, username) = (?, ?)", (service, username))
     results = cursor.fetchall()
+
+    # Checking if the password exists
     if not results:
         print("Password to delete does not exist. Returning to options...")
     else:
@@ -57,8 +65,9 @@ def delete_password(service, username):
     conn.commit()
     conn.close()
 
+# Function that returns password based on service and username input
 def get_password(service, username):
-    try:
+    try: # Try/except clause that throws exception if the (service, username) pair doesn't exist in the database
         print("Getting password...")
         conn = sqlite3.connect("password_storage.db")
         cursor = conn.cursor()
@@ -69,7 +78,7 @@ def get_password(service, username):
         decrypted_password = decrypt_password(results, aes_key)
         print("The password is:", decrypted_password)
     except:
-        print("Service/Username pair doesn't exist in database...")
+        print("(service, username) pair doesn't exist in database...")
 
 def list_services():
     print("Listing services...\n")
@@ -85,8 +94,9 @@ def list_services():
         for service in results:
             print(service)
 
+    
 
-# Test Case
+# Main
 if __name__ == "__main__":
     conn = sqlite3.connect("password_storage.db")
     cursor = conn.cursor()
@@ -95,7 +105,6 @@ if __name__ == "__main__":
     conn.close()
     
     aes_key = b'o+\xc3\xff\x00j\x0e\x07\xc8\xeb\xed\xd7\xb0\x04\x91\xbb' # hard-coded key only for demonstration
-    print(aes_key)
 
     while True:
         user_choice = input("\nSelect option: \n\n [1] Save Password \n [2] Delete Password \n [3] Get Password \n [4] List Services \n [5] Exit\n\n")
